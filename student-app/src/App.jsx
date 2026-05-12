da037b74a41d1a9f9ffb006d99a69ca27a5571b9
V-Devi-devi <devi331331@gmail.com>
2026-05-12 09:54:42 +0530

    initial commit

--- student-app/src/App.jsx
+++ student-app/src/App.jsx
@@ -9,7 +9,6 @@ import ProtectedRoute from './components/ProtectedRoute'
 import CreateStudentPage from './components/CreateStudentPage'
 import EditStudentPage from './components/EditStudentPage'
 import AiChatPage from './components/AiChatPage'
-import ChatPage from './components/ChatPage'
 
 export default function App() {
   return (
